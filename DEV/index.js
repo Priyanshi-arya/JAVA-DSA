02733b7a4e4fb957b3aa33131801e42ef4942263
Priyanshi-arya <priyanshiarya55@gmail.com>
2025-01-24 18:40:03 +0530

    prime no.

---
+++ DEV/index.js
@@ -0,0 +1,13 @@
+let number=10;
+let flag=true;
+for(let div=2;div<number;div++){
+    if(number%div==0){
+        flag=false;
+        break;
+    }
+}
+if(flag==true){
+    console.log(number, "is prime")
+}else{
+    console.log(number, "is not prime")
+}
