65fc5aacd3905b60beb6d1231089fa26c8f81bf9
Priyanshi-arya <priyanshiarya55@gmail.com>
2025-02-07 14:16:10 +0530

    pattern 10

--- DSA JAVA/patt9.java
+++ DSA JAVA/patt9.java
@@ -1,13 +1,17 @@
 import java.util.*;
-public class patt8 {
+public class patt9 {
     public static void main(String[] args) {
         Scanner scn= new Scanner(System.in);
         int n=scn.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                if(i+j == n+1){
+                if(i+j==n+1){
                     System.out.print("*\t");
-                }else{
+                }
+         else if (i==j){
+            System.out.print("*\t");
+        }
+                else{
                     System.out.print("\t");
                 }
             }
