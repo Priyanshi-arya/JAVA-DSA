a6f01c19d8c2f261627044e2dd6330a6e575ce52
Priyanshi-arya <priyanshiarya55@gmail.com>
2025-02-08 16:29:51 +0530

    pattern 16

---
+++ DSA JAVA/patt14.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class patt14 {
+    public static void main(String[] args) {
+        Scanner snc = new Scanner (System.in);
+        int n= snc.nextInt();
+        for (int i =1;i<=10;i++){
+        int val =n*i;
+         System.out.println(n+" * "+i+"="+" "+val);
+
+        }
+    }
+    
+}
